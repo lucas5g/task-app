ac7370134481741861b41a2b3bf0c11912975511
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2020-05-27 23:12:16 +0000

    my version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '@atlaskit/css-reset'
+import { DragDropContext } from 'react-beautiful-dnd'
+import initialData from './initial-data'
+import Column from './column'
+import styled from 'styled-components'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Container = styled.div`
+  display: flex;
+`
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const [data, setData] = useState(initialData)
+
+  const onDragEnd = result => {
+    //document.body.style.color = 'inherit'
+
+    const { destination, source, draggableId } = result
+
+    if (!destination)
+      return
+
+    if (destination.draggableId === source.droppableId && destination.index === source.index)
+      return
+
+    //const column = data.columns[source.droppableId]
+    const start = data.columns[source.droppableId]
+    const finish = data.columns[destination.droppableId]
+
+    if (start === finish) {
+      const newTaksIds = Array.from(start.taskIds)
+      newTaksIds.splice(source.index, 1)
+      newTaksIds.splice(destination.index, 0, draggableId)
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaksIds
+      }
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn
+        }
+      }
+
+      setData(newData)
+    }
+    
+    //Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds)
+    startTaskIds.splice(source.index, 1)
+    
+    const newStart = {
+      ...start, 
+      taskIds: startTaskIds
+    }
+
+    const finishTaskIds = Array.from(finish.taskIds)
+    finishTaskIds.splice(destination.index, 0, draggableId)
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds
+    }
+
+    const newData = {
+      ...data,
+      columns:{
+        ...data.columns,
+        [newStart.id]:newStart,
+        [newFinish.id]:newFinish
+      }
+    }
+
+    setData(newData)
+
+
+
+  }
+
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Container>
+        {console.log(data)}
+        {data.map(r => 
+          <Column key={r.id} />
+        )}
+        {/*data.columnOrder.map(columnId => {
+          const column = data.columns[columnId]
+          const tasks = column.taskIds.map(taskId => data.tasks[taskId])
+          return <Column key={column.id} column={column} tasks={tasks} />
+        })*/}
+      </Container>
+    </DragDropContext>
+
+  )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ src/task.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Draggable} from 'react-beautiful-dnd'
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragging ? 'lightgreen':'white')};
+  display: flex;
+`
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
+
+`
+
+function Task(props){
+  return (
+    <Draggable draggableId={props.task.id} index={props.index}>
+     
+      {(provided, snapshot) => (
+        <Container
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          >
+          <Handle {...provided.dragHandleProps} />
+          {props.task.content}
+        </Container>
+      )}
+    </Draggable>
+  ) 
+}
+
+export default Task
